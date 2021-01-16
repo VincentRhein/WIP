8ff20b98b7207f0c120382bd73bff8a15a5ed6a7
VincentRhein <59822197+VincentRhein@users.noreply.github.com>
2021-01-16 12:25:57 +0100

    new
    updated

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
